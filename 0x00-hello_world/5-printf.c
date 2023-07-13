47a25eaa8f8cd74e12dce72f95bf0e770a5ec710
tolossamuel <tolossamuel1@gmail.com>
2023-07-13 09:07:41 +0300

    Hello world printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * using of printf
+ * 
+ *lastly return must be 0
+*/
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
+}
