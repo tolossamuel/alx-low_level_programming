374809a2216eb0932effeaca6224bae01258f840
tolossamuel <tolossamuel1@gmail.com>
2023-07-13 09:04:43 +0300

    Hello world puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * batty style
- * return always 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
