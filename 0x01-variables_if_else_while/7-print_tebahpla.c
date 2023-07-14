22c44a5a7f7898a79bafdf61b1e20e95b74d8f2a
tolossamuel <tolossamuel1@gmail.com>
2023-07-14 11:47:36 +0300

    updated

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- print lower case alphabets in reverse orders
+ * Return: zerro 0 (seccuss)
+ */
+int main(void)
+{
+	int i;
+
+	char lettes[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'};
+for (i = 26; i > 0; i--)
+{
+	putchar(lettes[i - 1]);
+}
+}
