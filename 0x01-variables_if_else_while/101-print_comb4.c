554576e091f51a178b10b695778c5a0f5a770778
tolossamuel <tolossamuel1@gmail.com>
2023-07-14 13:48:22 +0300

    updated

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main- print all combinations of three digits
+ * Return: zero 0 (success)
+ */
+int main(void)
+{
+int n, m, l;
+
+for (n = 48; n < 58; n++)
+{
+	for (m = 49; m < 58; m++)
+	{
+		for (l = 50; l < 58; l++)
+		{
+			putchar(n);
+			putchar(m);
+			putchar(l);
+			if (n != 55 || m != 56)
+			{
+				putchar(44);
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar(' ');
+return (0);
+}
