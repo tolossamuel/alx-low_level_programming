b9cd4b4e1a3cd8caf7412aac30b7d0f7fcd6a093
tolossamuel <tolossamuel1@gmail.com>
2023-07-14 11:41:14 +0300

    updated

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- print digits
+ * only use putcher function not allow to use pritf
+ * Return: zerro 0 (success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+	putchar(i);
+}
+}
