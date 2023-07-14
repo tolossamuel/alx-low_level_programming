b0b0904338af966102af401386c5662a6b3b3149
tolossamuel <tolossamuel1@gmail.com>
2023-07-14 11:21:15 +0300

    updated

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main- print alphabets
+ * Return: Zerro 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+char letter[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+for (i = 0; i < 26; i++)
+{
+	putchar(letter[i]);
+}
+for (i = 0; i < 26; i++)
+{
+	letter[i] = toupper(letter[i]);
+	putchar(letter[i]);
+}
+}
