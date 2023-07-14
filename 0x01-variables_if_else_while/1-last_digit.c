571a3a6414ec88af5be6324bb3f8b11eeb234ae4
tolossamuel <tolossamuel1@gmail.com>
2023-07-14 11:04:36 +0300

    updated

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main- pritf
+ * Return: zero 0 (sss)
+ */
+int main(void)
+{
+int n;
+int a;
+
+srand(time(NULL) ^ getpid());
+n = rand() - RAND_MAX / 2;
+a = n;
+while (a > 10)
+{
+	a = a % 10;
+}
+
+if (a > 5)
+{
+	printf("%d is %d and is greater than 5\n", n, a);
+}
+else if (a < 6)
+{
+	printf("%d is %d and is less than 6 and not 0\n", n, a);
+}
+else
+{
+	printf("%d is %d and is 0\n", n, a);
+}
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- print letters
+ * Return: zero 0 (sus)
+ */
+int main(void)
+{
+char letter[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'};
+int i;
+for (i = 0; i < 26; i++)
+{
+	putchar(letter[i]);
+}
+}
