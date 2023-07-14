2a5e588319fc95de70afd7ac69f08e425c58bb31
tolossamuel <tolossamuel1@gmail.com>
2023-07-14 11:32:41 +0300

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- print digits
+ * the digits start from 0 up to 9
+ * Return: zerro 0 (success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	printf("%d", i);
+}
+}
