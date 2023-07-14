52071a2e0de7539775939429606aac6801039e7f
tolossamuel <tolossamuel1@gmail.com>
2023-07-14 11:26:30 +0300

    updated

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- print alphabets
+ * except e and q
+ * Return: zerro 0 (success)
+ */
+int main(void)
+{
+int i;
+
+char letter[] = {'a', 'b', 'c', 'd', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'};
+for (i = 0; i < 24; i++)
+{
+	putchar(letter[i]);
+}
+}
