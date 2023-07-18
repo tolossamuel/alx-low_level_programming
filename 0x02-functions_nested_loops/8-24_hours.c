8a920e06ac81e3f2bb37a0edab93b7ecad447621
tolossamuel <tolossamuel1@gmail.com>
2023-07-18 13:51:45 +0300

    updated

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * void jack_bauer - print evry minut
+ * Return: zero (0)
+ */
+void jack_bauer(void);
+{
+int n, m, x, y;
+
+for (n = 0; n < 3; n++)
+{
+	for (m = 0; m < 10; m++)
+	{
+		for (x = 0; x < 6; x++)
+		{
+			for (y = 0; y < 10; y++)
+			{
+				if (n == 2 && m == 3 && x == 5 && y == 9)
+				{
+					_putchar(n);
+					_putchar(m);
+					_putchar(':');
+					_putchar(x);
+					_putchar(y);
+					_putchar('\n');
+					return (0);
+				}
+				_putchar(n);
+				_putchar(m);
+				_putchar(':');
+				_putchar(x);
+				_putchar(y);
+				_putchar('\n');
+			}
+		}
+	}
+}
+return (0);
+}
