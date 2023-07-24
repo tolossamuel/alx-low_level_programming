f034f85c7cb260ae3ab6f2075b8bd7cad48aa6ca
tolossamuel <tolossamuel1@gmail.com>
2023-07-24 15:06:58 +0300

    updated

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - print hafe of the string
+ * @str: strings
+ */
+void puts_half(char *str)
+{
+	int i, x;
+
+	while (str[i] != '\0')
+	{
+		i += 1;
+	}
+	x = (i - 1) / 2;
+	while (x != i)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
