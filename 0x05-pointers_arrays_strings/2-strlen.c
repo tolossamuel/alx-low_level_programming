806fbd25507b93181390ee2df764e5d76b6b0703
tolossamuel <tolossamuel1@gmail.com>
2023-07-24 14:35:50 +0300

    updated

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - find the lenght of string
+ * @s: string
+ * Return: the value of length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
