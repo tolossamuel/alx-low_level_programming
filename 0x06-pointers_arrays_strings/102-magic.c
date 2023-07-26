925d30645ed3ee2549fc25d2cc726c56f65c0b40
tolossamuel <tolossamuel1@gmail.com>
2023-07-26 21:12:03 +0300

    updated

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - print 98
- * Return: zero (0)
-*/
 int main(void)
 {
 int n;
@@ -22,6 +18,6 @@ p = &n;
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
-printf("a[2] = %d\n", 98);
+printf("a[2] = %d\n", a[2]);
 return (0);
 }
