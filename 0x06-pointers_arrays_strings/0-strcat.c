7425b1fe11b6f7f5146fe9789893e08ef407a60f
tolossamuel <tolossamuel1@gmail.com>
2023-07-26 12:22:21 +0300

    updated

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatinate two strings
+ * @dest: string one
+ * @src: string two
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+it is more about poiters array and string alx low level programming project 6
