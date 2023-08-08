1af7705f92bec0ce67118cb56789d9e9874ae6dc
tolossamuel <tolossamuel1@gmail.com>
2023-08-08 10:56:55 +0300

    updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,13 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * create_array - alocate new momory place
- * @size: unsigned int
- * @c: value of variable
- * Return: memory alocations
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array to create
+ * @c: char to initialize the array c
+ *
+ * Return: pointer to the array (Success), NULL (Error)
  */
 char *create_array(unsigned int size, char c)
 {
@@ -13,19 +15,20 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
+
 	p = (char *) malloc(sizeof(char) * size);
+
 	if (p == NULL)
-	{
 		return (0);
-	}
+
 	while (i < size)
 	{
-		*(p + 1) = c;
+		*(p + i) = c;
 		i++;
 	}
-	*(p + 1) = '\0';
+
+	*(p + i) = '\0';
+
 	return (p);
 }
