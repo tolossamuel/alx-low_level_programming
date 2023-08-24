f95818ecc519669139ae37be32fd13481d851b4d
tolossamuel <tolossamuel1@gmail.com>
2023-08-24 23:08:13 +0300

    print linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 {
 size_t element;
 
-element = 0;
+element = 1;
 if (h == NULL)
 {
 	return (0);
@@ -22,11 +22,11 @@ while (h->next != NULL)
 		printf("[%d] %s\n", 0, "(nil)");
 	} else
 	{
-		printf("[%lu] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 	}
 	h = h->next;
 	element += 1;
 }
-printf("[%lu] %s\n", h->len, h->str);
+printf("[%d] %s\n", h->len, h->str);
 return (element);
 }
