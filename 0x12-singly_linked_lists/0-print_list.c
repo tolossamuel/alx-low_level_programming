64a17812ca22d4ff3ab5cc58084348dcf3f8a922
tolossamuel <tolossamuel1@gmail.com>
2023-08-24 22:48:40 +0300

    linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "samy.h"
+
+/**
+ * print_list - print element of linked list
+ * @h: node of the linked list
+ * Return: element
+ */
+size_t print_list(const list_t *h)
+{
+size_t element;
+
+element = 0;
+while (h != NULL)
+{
+	if (h->str != NULL)
+	{
+		printf("[%lu] %s\n", h->len, h->str);
+	} else
+	{
+		printf("[0] (nil)\n");
+	}
+h = h->next;
+element++;
+}
+return (element);
+}
---
+++ 0x12-singly_linked_lists/samy.h
@@ -0,0 +1,7 @@
+#ifndef SAMY_H
+#define SAMY_H
+#include <stddef.h>
+
+size_t print_list(const list_t *h);
+
+#endif
