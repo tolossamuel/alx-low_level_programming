712ec34bf9188416db713e5a0b6ff2a7e4ea464f
tolossamuel <tolossamuel1@gmail.com>
2023-08-25 00:22:16 +0300

    holbertt

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,22 @@
-section .data
-    hello db "Hello, Holberton,", 0
-    format db "%s", 10, 0
+; Declare needed C  functions
+	extern	printf		; the C function, to be called
 
-section .text
-    global main
-    extern printf
+	section .data		; Data section, initialized variables
+msg:	db "Hello, Holberton", 0; C string needs 0
+fmt:	db "%s", 10, 0		; The printf format, "\n",'0'
 
-main:
-    ; Prepare arguments and call printf
-    mov rdi, format
-    mov rsi, hello
-    xor rax, rax  ; Clear RAX register for proper printf call
-    call printf
+	section .text		; Code section.
 
-    ; Exit the program
-    xor rdi, rdi  ; Return code 0
-    mov rax, 60   ; syscall: exit
-    syscall
+	global main		; the standard gcc entry point
+main:				; the program label for the entry point
+	push	rbp		; set up stack frame, must be alligned
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0		; or can be  xor  rax,rax
+	call	printf		; Call C function
+
+	pop	rbp		; restore stack
+
+	mov	rax,0		; normal, no error, return value
+	ret			; return
