249b573a7a3f999b6db7a159f9e3367438769393
tolossamuel <tolossamuel1@gmail.com>
2023-08-28 23:51:27 +0300

    updated

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,12 +7,11 @@
 */
 void free_listint(listint_t *head)
 {
-listint_t *tmp;
+	listint_t *temp;
 
-while ((temp = head) != NULL)
-{
-head = head->next;
-temp = head
-free(tmp);
-}
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
 }
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,16 @@
 */
 void free_listint2(listint_t **head)
 {
-listint_t *tmp;
-
-while (*head)
+listint_t *tmp, *index;
+if (*head != NULL)
 {
-tmp = *head;
-*head = (*head)->next;
+index = *head;
+while (index != NULL)
+{
+
+tmp = index;
+index = index->next;
 free(tmp);
+}
 }
 }
