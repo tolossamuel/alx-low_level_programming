be66d8e727d82434ad94b7ef41d90f88e4e4b55e
tolossamuel <tolossamuel1@gmail.com>
2023-08-29 01:14:57 +0300

    updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,40 +1,39 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - insert new node
+ * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: head of list
  * @idx: index
  * @n: int
  * Return: listint_t*
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *current, *index;
-	unsigned int count = 0;
+listint_t *new_node;
+listint_t *current;
+unsigned int count = 0;
 
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
-		return (NULL);
-	temp->n = n;
-	index = *head;
-	if (idx == 0)
-		temp->next = index;
-		*head = temp;
-		return (temp);
-	while (index)
-		if (count + 1 == idx)
-			temp->next = index->next;
-			index->next = temp;
-			return (temp);
-		count++;
-	count = 0;
-	current = *head->next;
-	index = head;
-	while (count <= index)
-		index = index->next;
-		current = current->next;
-	current->next = temp;
-	temp->next = index;
-	return (*head);
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+	return (NULL);
+new_node->n = n;
+current = *head;
+if (idx == 0)
+{
+	new_node->next = current;
+	*head = new_node;
+	return (new_node);
+}
+while (current)
+{
+	if (count + 1 == idx)
+	{
+		new_node->next = current->next;
+		current->next = new_node;
+		return (new_node);
+	}
+	count++;
+	current = current->next;
+}
+return (NULL);
 }
