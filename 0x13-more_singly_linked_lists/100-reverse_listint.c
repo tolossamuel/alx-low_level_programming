ce14286f7883e13f39f94f0ca4f8c3cfd9a6cf35
tolossamuel <tolossamuel1@gmail.com>
2023-08-29 11:41:49 +0300

    more about linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
  * reverse_listint - function that reverses a listint_t linked list.
- * You are not allowed to use malloc, free or arrays
- * You can only declare a maximum of two variables in your function
  * @head: pointer to the first node
+ * 
  * Return: pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
