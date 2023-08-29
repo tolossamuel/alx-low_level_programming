24666b87d08748783816331f56840499c487d284
tolossamuel <tolossamuel1@gmail.com>
2023-08-29 11:37:41 +0300

    more about linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,41 +8,34 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *new_node, *current;
+	listint_t *new_node, *current;
 	unsigned int index;
 
-	current = *head; /*place first node at current*/
-
+	current = *head;
 	new_node = malloc(sizeof(listint_t));
 	if ((*head == NULL && idx != 0) || new_node == NULL)
 		return (NULL);
-
-	new_node->n = n; /* add our element to the new node*/
-
-	/*iterate list to node position idx - 2*/
+	new_node->n = n;
 	for (index = 0; head != NULL && index < idx - 1; index++)
 	{
 		current = current->next;
 		if (current == NULL)
 			return (NULL);
 	}
-
-	if (idx == 0) /*if the index for new node is 0*/
+	if (idx == 0)
 	{
-		/*first node will be moved to second node*/
 		new_node->next = *head;
-		/*new node will be placed as the first node*/
 		*head = new_node;
 	}
-	else if (current->next) /*if index where to add our new node is not 0*/
+	else if (current->next)
 	{
-		new_node->next = current->next; /*place current node after new node*/
-		current->next = new_node;/*set the new node at index idx*/
+		new_node->next = current->next;
+		current->next = new_node;
 	}
-	else /*if node position is not present in the list*/
+	else
 	{
-		new_node->next = NULL;/*set next addr as NULL, indicates last node*/
-		current->next = new_node;/*set the new node at the last position in list*/
+		new_node->next = NULL;
+		current->next = new_node;
 	}
 
 	return (new_node);
