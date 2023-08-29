68577beea1b820fb6949c10bed888354b7e6394a
tolossamuel <tolossamuel1@gmail.com>
2023-08-29 13:40:52 +0300

    more about linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,20 +3,19 @@
 /**
  * find_listint_loop - finds the loop in a linked list.
  * @head: head of a list.
- *
  * Return: the address of the node where the loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
-    listint_t *fast = head;
+	listint_t *fast = head;
 
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast)
-            return (slow);
-    }
-    return (NULL);
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (slow);
+	}
+	return (NULL);
 }
