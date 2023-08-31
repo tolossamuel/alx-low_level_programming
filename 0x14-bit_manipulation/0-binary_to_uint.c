5e29249a13147a5df91dd2777580a7680e4304bd
tolossamuel <tolossamuel1@gmail.com>
2023-08-31 22:22:56 +0300

    bit manipulations

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
+	unsigned int numbers;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	while (*b != '\0')
+
+	numbers = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (*b == '0')
-			res = res << 1;
-		else if (*b == '1')
-			res = res << 1 | 1;
-		else
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		b++;
+		}
+
+		if (b[len] & 1)
+		{
+			numbers += base_two;
+		}
 	}
-	return (res);
+
+	return (numbers);
 }
