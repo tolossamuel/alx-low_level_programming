0d50c07e58d5677a48917c7462fbb99c82a979e8
tolossamuel <tolossamuel1@gmail.com>
2023-08-31 21:58:36 +0300

    bit manipulations

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index < 63)
-		if (n & (1 << index))
+	unsigned int num;
+
+	if (index <= 63)
+	num = (1 << index);
+		if (n & num)
 		{
-			*n ^= (1 << index);
+			*n ^= num;
 		}
 		return (1);
 	return (-1);
