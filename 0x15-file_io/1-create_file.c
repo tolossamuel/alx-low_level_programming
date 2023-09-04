5f5cb4410ff926887c4249bc1930d6f7ef3c4e48
tolossamuel <tolossamuel1@gmail.com>
2023-09-04 16:35:42 +0300

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (size = 0; text_content[size];)
-			len++;
+			size++;
 	}
 	open_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	write_file = write(open_file, text_content, size);
