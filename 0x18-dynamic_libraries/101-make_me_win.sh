d992aa6adb89d284a3471a1d959b3054b1853aba
tolossamuel <tolossamuel1@gmail.com>
2023-10-16 23:53:46 +0300

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/tolossamuel/alx-low_level_programming/raw/master/0x18-dynamic_libraries/liball.so
-export LD_PRELOAD=/tmp/nrandom.so
+export LD_PRELOAD=/tmp/liball.so
